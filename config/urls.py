1a3e99354c83b71df13addb83994445d3895fe2b
gcs <iusegithub@tuta.com>
2026-07-27 02:58:30 +0900

    admin page

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("scholarships.urls")),
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
 ]
